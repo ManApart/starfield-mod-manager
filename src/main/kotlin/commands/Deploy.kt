77910778dc28bd5a360fecb14ac1207cdd035ac6
ManApart <r@austinkucera.com>
2023-10-08 14:14:14 -0400

    deploy soft link

--- src/main/kotlin/commands/Deploy.kt
+++ src/main/kotlin/commands/Deploy.kt
@@ -1,7 +1,9 @@
 package commands
 
+import toolConfig
 import toolState
 import java.io.File
+import java.nio.file.Files
 
 fun deployHelp(args: List<String>) = """
 """.trimIndent()
@@ -11,7 +13,9 @@ fun deploy(args: List<String>) {
     if (files.isEmpty()) {
         println("No mod files found")
     } else {
-        println("Found Mod Files:\n${files.entries.joinToString("\n") { (key, file) -> "$key: ${file.path}" }}")
+//        println("Found Mod Files:\n${files.entries.joinToString("\n") { (key, file) -> "$key: ${file.path}" }}")
+        files.entries.forEach { (gamePath, modFile) -> makeLink(gamePath, modFile) }
+        println("Deployed ${files.size} files")
     }
 }
 
@@ -24,4 +28,22 @@ private fun getAllModFiles(): Map<String, File> {
         }
     }
     return mappings
+}
+
+fun makeLink(gamePath: String, modFile: File) {
+    val gameFile = File(toolConfig.gamePath + "/$gamePath")
+    gameFile.parentFile.mkdirs()
+    if (Files.isSymbolicLink(gameFile.toPath())) {
+        val existingLink = Files.readSymbolicLink(gameFile.toPath())
+        if (existingLink != modFile.canonicalFile.toPath()) {
+            println("Update link ${modFile.path}")
+            gameFile.delete()
+            Files.createSymbolicLink(gameFile.toPath(), modFile.canonicalFile.toPath())
+        }
+    } else if (gameFile.exists()) {
+        //TODO - eventually backup real file and create link
+        println("Skipping replacing real file ${modFile.path}")
+    } else {
+        Files.createSymbolicLink(gameFile.toPath(), modFile.canonicalFile.toPath())
+    }
 }
