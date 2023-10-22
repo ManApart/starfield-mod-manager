061033f95f95a1fa44741ecf4bc24ed454830544
ManApart <r@austinkucera.com>
2023-10-22 10:08:31 -0400

    wip collection notes

---
+++ src/main/kotlin/commands/Collection.kt
@@ -0,0 +1,10 @@
+package commands
+
+fun collectionHelp() = """
+    collection list
+    collection save
+    collection load
+""".trimIndent()
+
+fun collection(args: List<String> = listOf()) {
+}
