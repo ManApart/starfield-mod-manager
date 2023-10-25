d871646c29845d7317e5aef4b1de2207a76b5e06
ManApart <r@austinkucera.com>
2023-10-25 09:09:07 -0400

    refresh staged

--- src/main/kotlin/commands/Refresh.kt
+++ src/main/kotlin/commands/Refresh.kt
@@ -11,26 +11,34 @@ fun refreshHelp() = """
     refresh 1-3
     refresh all - For all mods with ids, attempt to redownload (or grab the file from the downloads folder if it exists) and restage.
     refresh empty - Refresh any files without staged data
+    refresh staged - Refresh only files that are staged
     If you're looking to upgrade to a new version, see update and upgrade
 """.trimIndent()
 
 fun refresh(args: List<String>) {
-    if (args.isEmpty()) {
-        println(refreshHelp())
-    } else if (args.first() == "empty"){
-        toolData.mods
-            .filter { !File(it.filePath).exists() }
-            .refreshMods()
-    } else {
-        args.getIndicesOrRange(toolData.mods.size)
-            .mapNotNull { toolData.byIndex(it) }
-            .refreshMods()
+    when {
+        args.isEmpty() -> println(refreshHelp())
+        args.first() == "empty" -> {
+            toolData.mods
+                .filter { !File(it.filePath).exists() }
+                .refreshMods()
+        }
+        args.first() == "staged" -> {
+            toolData.mods
+                .filter { File(it.filePath).exists() }
+                .refreshMods()
+        }
+        else -> {
+            args.getIndicesOrRange(toolData.mods.size)
+                .mapNotNull { toolData.byIndex(it) }
+                .refreshMods()
+        }
     }
 }
 
-private fun List<Mod>.refreshMods(){
+private fun List<Mod>.refreshMods() {
     filter { it.id != null }
         .also { println("Refreshing ${it.size} mods") }
         .forEach { addModById(it.id!!, it.fileId) }
-        println("Done Refreshing")
+    println("Done Refreshing")
 }
