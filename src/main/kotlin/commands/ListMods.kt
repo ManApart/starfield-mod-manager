3fcda5ddb98169c464657f839b6a62442159040d
ManApart <r@austinkucera.com>
2023-11-05 09:18:39 -0500

    combine status columns

--- src/main/kotlin/commands/ListMods.kt
+++ src/main/kotlin/commands/ListMods.kt
@@ -46,20 +46,18 @@ fun display(mods: List<Pair<Mod, Boolean>>) {
         Column("Id", 7),
         Column("Version", 12),
         Column("Load", 7, true),
-        Column("Staged", 8),
-        Column("Enabled", 9),
-        Column("Endorsed", 10),
+        Column("Status", 10),
         Column("Category", 20),
         Column("Index", 7, true),
         Column("Name", 22),
     )
     val data = mods.mapIndexed { i, (mod, displayed) ->
         with(mod) {
-            val enabledCheck = if (enabled) ENABLED else " "
+            val enabledCheck = if (enabled) ENABLED else "  "
             val endorsedCheck = when(endorsed){
                 true -> THUMBS_UP
                 false -> THUMBS_DOWN
-                else -> " "
+                else -> "  "
             }
             val idClean = id?.toString() ?: "?"
             val versionClean = when {
@@ -68,13 +66,11 @@ fun display(mods: List<Pair<Mod, Boolean>>) {
                 latestVersion != null -> "$UPDATE?"
                 else -> "  ?"
             }
-            val staged = if (File(filePath).exists()) FOLDER else " "
+            val staged = if (File(filePath).exists()) FOLDER else "  "
             val category = category() ?: "?"
             mapOf(
                 "Index" to i,
-                "Staged" to staged,
-                "Enabled" to enabledCheck,
-                "Endorsed" to endorsedCheck,
+                "Status" to "$staged $enabledCheck $endorsedCheck",
                 "Load" to loadOrder,
                 "Id" to idClean,
                 "Version" to versionClean,
