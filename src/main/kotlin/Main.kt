a1f92c5ae54d36c573ea43f6ed7ecdd80d541604
ManApart <r@austinkucera.com>
2023-11-19 10:26:26 -0500

    allow custom config path using XDG_CONFIG_HOME

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,11 +7,16 @@ lateinit var toolData: Data
 lateinit var toolConfig: Config
 lateinit var modFolder: File
 val HOME = System.getProperty("user.home")!!
+val CONFIG_PATH = configPath()
 var confirmation: ((List<String>) -> Unit)? = null
 
 val jsonMapper = kotlinx.serialization.json.Json {
     ignoreUnknownKeys = true
     prettyPrint = true
+}
+
+private fun configPath(): String {
+    return System.getenv("XDG_CONFIG_HOME")?.replace("~", HOME)?.let { "$it/starfield-mod-manager-config.json" } ?: "./config.json"
 }
 
 fun main(args: Array<String>) {
@@ -37,7 +42,7 @@ fun main(args: Array<String>) {
 }
 
 private fun loadData() {
-    toolConfig = File("./config.json").takeIf { it.exists() }?.let {
+    toolConfig = File(CONFIG_PATH).takeIf { it.exists() }?.let {
         jsonMapper.decodeFromString(it.readText())
     } ?: Config()
     toolData = File("./data.json").takeIf { it.exists() }?.let {
@@ -47,7 +52,7 @@ private fun loadData() {
 }
 
 fun save() {
-    File("./config.json").writeText(jsonMapper.encodeToString(toolConfig))
+    File(CONFIG_PATH).writeText(jsonMapper.encodeToString(toolConfig))
     File("./data.json").writeText(jsonMapper.encodeToString(toolData))
 }
 
