4f0afcaf2ff070a11624d0774fde5e45c2de89b6
ManApart <r@austinkucera.com>
2024-01-14 10:51:26 -0500

    manually set version

--- src/main/kotlin/commands/Version.kt
+++ src/main/kotlin/commands/Version.kt
@@ -1,30 +1,42 @@
 package commands
 
 import Mod
+import save
 import toolData
 
 val versionDescription = """
     View the current and latest version of a mod
+    Use version set to manually tell the manager that mode is at a given version
 """.trimIndent()
 val versionUsage = """
     version <index>
     version 1 2 4
     version 1-3
+    version <index> set <new-version>
 """.trimIndent()
 
 fun version(args: List<String>) {
-    if (args.isEmpty()) {
-        toolData.mods.viewVersion()
-    } else {
-        args.getIndicesOrRange(toolData.mods.size)
-            .mapNotNull { toolData.byIndex(it) }
-            .viewVersion()
+    when {
+        args.isEmpty() -> toolData.mods.viewVersion()
+        args.size == 3 && args[1] == "set" -> setVersion(args[0].toInt(), args.drop(2).joinToString(" "))
+        else -> {
+            args.getIndicesOrRange(toolData.mods.size)
+                .mapNotNull { toolData.byIndex(it) }
+                .viewVersion()
+        }
     }
 }
 
-private fun List<Mod>.viewVersion() {
-    forEach { mod ->
-        println("${mod.index} ${mod.version} -> ${mod.latestVersion} ${mod.name}")
-    }
+private fun List<Mod>.viewVersion() = forEach { it.viewVersion() }
 
+private fun Mod.viewVersion() {
+    println("$index $version -> $latestVersion $name")
+}
+
+private fun setVersion(i: Int, version: String) {
+    toolData.byIndex(i)?.let { mod ->
+        mod.version = version
+        save()
+        mod.viewVersion()
+    }
 }
