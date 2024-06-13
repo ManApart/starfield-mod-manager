efc49d9eb83bbf1e6630af2ed646402487f5b653
ManApart <r@austinkucera.com>
2024-06-13 08:36:51 -0400

    creation displays friendly name if possible

--- src/main/kotlin/commands/Creation.kt
+++ src/main/kotlin/commands/Creation.kt
@@ -8,8 +8,9 @@ import save
 import toolConfig
 import toolData
 import yellow
+import java.io.BufferedReader
 import java.io.File
-import java.nio.file.CopyOption
+import java.io.InputStreamReader
 import java.nio.file.Files
 import java.nio.file.StandardCopyOption
 
@@ -26,6 +27,15 @@ val creationUsage = """
    creation add all
 """.trimIndent()
 
+val idMap by lazy {
+    ClassLoader.getSystemClassLoader().getResourceAsStream("creationIds.txt")
+        .let { BufferedReader(InputStreamReader(it!!)).lines().toList() }
+        .associate { line ->
+            val (key, value) = line.split("=")
+            key.lowercase() to value.lowercase().replace(" ", "-")
+        }
+}
+
 fun creation(args: List<String>) {
     val firstArg = args.firstOrNull() ?: ""
     when {
@@ -38,17 +48,20 @@ fun creation(args: List<String>) {
     }
 }
 
-private fun listCreations() = getUnmanagedCreations().sorted()
-    .joinToString(", ").let { println(it) }
+private fun listCreations() = println(getUnmanagedCreations().sorted().creationFriendlyNames())
 
 private fun addAllCreations() {
     val creations = getUnmanagedCreations()
-    println(yellow("Add unmanaged creations? ") +creations.joinToString(", ") + " (y/n)")
+    println(yellow("Add unmanaged creations? ") + creations.toList().creationFriendlyNames() + " (y/n)")
     confirmation = { c ->
         if (c.firstOrNull() == "y") {
             creations.forEach { addCreation(it) }
         }
     }
+}
+
+private fun List<String>.creationFriendlyNames(): String {
+    return joinToString(", ") { it + (idMap[it.lowercase()]?.let { name -> " ($name)" } ?: "") }
 }
 
 private fun getUnmanagedCreations(): Set<String> {
@@ -63,7 +76,7 @@ fun addCreation(creationId: String, nameOverride: String? = null) {
     if (files.isEmpty()) {
         println(red("No files found for $creationId"))
     }
-    val name = nameOverride ?: creationId
+    val name = nameOverride ?: idMap[creationId] ?: creationId
 
     val existing = toolData.byName(name, true)
     val mod = if (existing != null) existing else {
